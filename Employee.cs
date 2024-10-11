8c397611a577a08447189ee35d301d20997082e3
andrey kapitovskiy <pepsicooled@mail.ru>
2024-10-11 10:46:48 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Employees
+{
+    public class Employee
+    {
+        [JsonPropertyName("name")]
+        public String name { get; set; }
+        [JsonPropertyName("phoneNumber")]
+        public String phoneNumber { get; set; }
+        [JsonPropertyName("salary")]
+        public String salary { get; set; }
+        [JsonIgnore]
+        public int numberSalary { get; set; }
+        public void ToFormate()
+        {
+            FormateTelephone();
+            FormateSalary();
+        }
+        private void FormateTelephone()
+        {
+            try
+            {
+                phoneNumber = '+' + phoneNumber.Substring(0, 1)
+                + '(' + phoneNumber.Substring(1, 3) + ')' + phoneNumber.Substring(4, 3)
+                + '-' + phoneNumber.Substring(7, 2) + '-' + phoneNumber.Substring(9, 2);
+               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine($"неудалось отформатировать номер телефона {phoneNumber} этого сотрудника {name}");
+            }
+        }
+        private void FormateSalary()
+        {
+            String temp = salary;
+            numberSalary = int.Parse(salary);
+            salary = String.Empty;
+            temp = string.Join("", temp.Reverse());
+            int i = 0;
+            while (i < temp.Length)
+            {
+
+                if (temp.Length - i > 3)
+                {
+                    salary += temp.Substring(i, 3) + " ";
+                    i += 3;
+                }
+                else
+                {
+                    salary += temp[i..(temp.Length)];
+                    break;
+                }
+
+            }
+            salary = String.Join("", salary.Reverse()) + " р";
+        }
+           
+        public override String ToString()
+        {
+            return $"{name} {phoneNumber}, {salary}";
+
+        }
+
+
+
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Employees;
+using System.Text.Json;
+String pathJsonFile = "..\\..\\..\\data.json";
+String pathTextFile = "..\\..\\..\\top_employeers";
+/*
+Директория из которой происходит запуск приложения находится в ./Employee/bin/Debug/net8.0/ 
+по этому я возвращаюсь на несколько директорий назад, что бы хранить статические файлы в той же директории где я работаю, для удобства"
+ */
+String JsonString = File.ReadAllText(pathJsonFile);
+List<Employee> employees = JsonSerializer.Deserialize<List<Employee>>(JsonString)!;
+employees.ForEach(e =>e.ToFormate());
+employees = employees.OrderByDescending(c=>c.numberSalary).Take(20).ToList();
+string projectDirectory = AppDomain.CurrentDomain.BaseDirectory;
+Console.WriteLine(projectDirectory);
+Directory.SetCurrentDirectory(projectDirectory);
+using (StreamWriter writer = new StreamWriter(pathTextFile))
+{
+    foreach(Employee employee in employees)
+    {
+        writer.WriteLine(employee.ToString());
+    }
+}
